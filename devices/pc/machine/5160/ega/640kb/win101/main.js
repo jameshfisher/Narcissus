497582e83971cb29d06bce727543f5bbb43e76e5
James Fisher <jhf@trifork.com>
2015-12-08 12:15:31 -0400

    hack to get the canvas

---
+++ devices/pc/machine/5160/ega/640kb/win101/main.js
@@ -0,0 +1,19 @@
+(function () {
+  var waitFor = function (pred, cb) {
+    var result = pred();
+    if (result != null) {
+      cb(result);
+    } else {
+      setTimeout(waitFor, 100, pred, cb);
+    }
+  };
+  waitFor(
+    function () {
+      // Seriously, who puts a period in a CSS id?
+      return document.querySelector("div#ibm5160\\.videoEGA canvas.pcjs-canvas");
+    },
+    function (canvasElement) {
+      console.log(canvasElement);
+    }
+  );
+})();
